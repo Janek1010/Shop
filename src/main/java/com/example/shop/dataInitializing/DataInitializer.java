d8e73026b94736a52b1ddceff220c1c49e495185
Janek1010 <janekdomozych99@gmail.com>
2023-10-18 20:55:47 +0200

    aaaa

--- src/main/java/com/example/shop/dataInitializing/DataInitializer.java
+++ src/main/java/com/example/shop/dataInitializing/DataInitializer.java
@@ -2,17 +2,17 @@ package com.example.shop.dataInitializing;
 
 import com.example.shop.customer.entities.Customer;
 import com.example.shop.customer.entities.Order;
-import com.example.shop.customer.services.CustomerService;
-import com.example.shop.customer.services.OrderService;
-import jakarta.annotation.PostConstruct;
+import com.example.shop.customer.services.api.CustomerService;
+import com.example.shop.customer.services.api.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
 
 @Component
 @RequiredArgsConstructor
-public class DataInitializer {
+public class DataInitializer implements InitializingBean {
     private static final List<String> products = List.of("Piwo IPA", "Piwo APA", "Pomidor", "Mango", "Młotek");
     private static final List<String> names = List.of("Jan", "Zbigniew", "Jacek", "Marek", "Jurek");
     private static final List<String> surnames = List.of("Kowalski", "Noga", "Jackowski", "Markowski", "Jurkowski");
@@ -22,8 +22,8 @@ public class DataInitializer {
     private final OrderService orderServiceJPA;
 
 
-    @PostConstruct
-    public void initializeData() {
+    @Override
+    public void afterPropertiesSet() throws Exception {
         Random rand = new Random();
         int peselIndex = -1;
         int surnameIndex = -1;
