648230420b5a8ef9ce9457e3dd3ad12fc877d37a
Janek1010 <janekdomozych99@gmail.com>
2023-11-12 10:31:58 +0100

    zrobione ladniutko wszystko

--- shop-gateway/src/main/java/com/example/shop/Config.java
+++ shop-gateway/src/main/java/com/example/shop/Config.java
@@ -8,25 +8,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Config {
 
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("customer-service", r -> r
-                        .path("/api/customers/{customerId}")
-                        .or()
-                        .path("/api/customers")
-                        .uri("http://localhost:8082")
-                )
-                .route("order-service", r -> r
-                        .path("/api/orders")
-                        .or()
-                        .path("/api/orders/**")
-                        .or()
-                        .path("/api/customers/{customerId}/orders")
-                        .or()
-                        .path("/api/customers/{customerId}/orders/**")
-                        .uri("http://localhost:8081")
-                )
-                .build();
-    }
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("customer-service", r -> r
+//                        .path("/api/customers/{customerId}")
+//                        .or()
+//                        .path("/api/customers")
+//                        .uri("http://localhost:8082")
+//                )
+//                .route("order-service", r -> r
+//                        .path("/api/orders")
+//                        .or()
+//                        .path("/api/orders/**")
+//                        .or()
+//                        .path("/api/customers/{customerId}/orders")
+//                        .or()
+//                        .path("/api/customers/{customerId}/orders/**")
+//                        .uri("http://localhost:8081")
+//                )
+//                .build();
+//    }
 }
