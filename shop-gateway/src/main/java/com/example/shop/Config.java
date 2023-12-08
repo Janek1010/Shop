c6a3b290300e65743eb53469611f70add3378b07
Janek1010 <janekdomozych99@gmail.com>
2023-12-08 23:34:02 +0100

    docker customer service

--- shop-gateway/src/main/java/com/example/shop/Config.java
+++ shop-gateway/src/main/java/com/example/shop/Config.java
@@ -1,23 +1,40 @@
 package com.example.shop;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class Config {
 
     @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+    public RouteLocator routeLocator(RouteLocatorBuilder builder,
+                                     @Value("${shop.customer.url}") String customerUrl,
+                                     @Value("${shop.order.url}") String orderUrl,
+                                     @Value("${shop.gateway.host}") String host
+
+                                     ) {
         return builder.routes()
                 .route("customer-service", r -> r
+                        .host(host)
+                        .and()
                         .path("/api/customers/{customerId}")
                         .or()
                         .path("/api/customers")
-                        .uri("http://localhost:8082")
+                        .uri(customerUrl)
                 )
                 .route("order-service", r -> r
+                        .host(host)
+                        .and()
                         .path("/api/orders")
                         .or()
                         .path("/api/orders/**")
@@ -25,8 +42,22 @@ public class Config {
                         .path("/api/customers/{customerId}/orders")
                         .or()
                         .path("/api/customers/{customerId}/orders/**")
-                        .uri("http://localhost:8081")
+                        .uri(orderUrl)
                 )
                 .build();
+    }
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
     }
 }
